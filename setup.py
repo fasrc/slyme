32319dac0116892c65c46f84713e747d5dfb396b
Aaron Kitzmiller <aaronkitzmiller@rc-akitzmiller-1.home>
2014-05-26 11:11:59 -0400

    setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -6,17 +6,22 @@ All rights reserved.
 
 @author: Aaron Kitzmiller
 """
-
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 
 setup(
-    name='slyme',
-    version='0.1.0',
+    name = "slyme",
+    version = "0.1.0",
     author='John Brunelle <john_brunelle@harvard.edu>, Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>',
     author_email='aaron_kitzmiller@harvard.edu',
-    packages=['slyme', 'test'],
-    url='http://pypi.python.org/pypi/Slyme/',
+    description='Python modules for wrapping the Slurm tools',
     license='LICENSE.txt',
-    description='Slurm Python modules using the executables',
+    keywords = "slurm",
+    url='http://pypi.python.org/pypi/slyme/',
+    packages = find_packages(),
     long_description=open('README.txt').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
 )
