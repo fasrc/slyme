83005c86b00159e3045a2dba266544cae9d6bd11
John Brunelle <john_brunelle@harvard.edu>
2014-07-17 17:00:58 -0400

    add bin/ to PATH

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-export PYTHONPATH="$(readlink -e $(dirname $BASH_SOURCE)):$PYTHONPATH"
+SLYME_ROOT="$(readlink -e $(dirname $BASH_SOURCE))"
+export PYTHONPATH="$SLYME_ROOT:$PYTHONPATH"
+export PATH="$SLYME_ROOT/bin:$PATH"
